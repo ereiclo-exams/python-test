d99829317ff49ee35248149ed54c1d3b6570e9be
Ereiclo <eric.bracamonte@utec.edu.pe>
2022-07-11 17:17:38 -0500

    added tests

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
 from datetime import datetime
 from multiprocessing.sharedctypes import Value
-class DateException(ValueError):
-    """Exception raised for errors in the input salary.
+class DateException(Exception):
 
-    Attributes:
-        salary -- input salary which caused the error
-        message -- explanation of the error
-    """
     __module__ = 'builtins'
 
-    def __init__(self, date, message="Salary is not in (5000, 15000) range"):
+    def __init__(self, date, consult_date, message=""):
+        self.date = date
+        self.consult_date = consult_date
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f'Date {self.consult_date} is less than birth_date ({self.date})'
+
+class DateFormatException(ValueError):
+
+    __module__ = 'builtins'
+
+    def __init__(self, date, message=""):
         self.date = date
         self.message = message
         super().__init__(self.message)
 
     def __str__(self):
-        return f'{self.salary} -> {self.message}'
-
-
+        return f'Date wrong format: {self.date}'
 
 
 def mayoria_de_edad(fecha_nacimiento,fecha_consulta):
-    # dt1 = datetime.strptime(fecha_nacimiento,'%d/%m/%Y')
+    try:
+        dt1 = datetime.strptime(fecha_nacimiento,'%d/%m/%Y')
+    except:
+        raise DateFormatException(fecha_nacimiento)
+    
 
-    # dt2 = datetime.strptime(fecha_nacimiento,'%d/%m/%Y')
-    pass
+    try:
+        dt2 = datetime.strptime(fecha_consulta,'%d/%m/%Y')
+    except:
+        raise DateFormatException(fecha_consulta)
+
+    if dt2 < dt1:
+        raise DateException(dt1,dt2)
+
+    return int((dt2 - dt1).days / 365) >= 18
+
+
+
--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
-from main import DateException
+from main import DateException,DateFormatException,mayoria_de_edad
 import pytest
 
 
@@ -7,17 +7,28 @@ class TestClass:
 
 
     def test_1(self):
-        with pytest.raises(DateException) as e_info:
+        with pytest.raises(Exception):
             raise DateException(2134)
         
-        # assert e_info.type is DateException 
-        # assert e_info.type is ValueError
-
 
     def test_2(self):
-        with pytest.raises(Exception) as e_info:
-            raise DateException(2134)
-            
-        assert e_info.type is DateException 
+        with pytest.raises(DateFormatException):
+            raise DateFormatException("")
 
+    def test_3(self):
+        assert mayoria_de_edad('18/05/2002','11/07/2022') == True
 
+    def test_4(self):
+        assert mayoria_de_edad('18/05/2002','11/07/2004') == False
+
+    def test_5(self):
+        with pytest.raises(DateException):
+            mayoria_de_edad('18/05/2002','18/02/2002')
+
+    def test_6(self):
+        with pytest.raises(DateFormatException):
+            mayoria_de_edad('18052002','18/06/2002')
+
+    def test_7(self):
+        with pytest.raises(DateFormatException):
+            mayoria_de_edad('18/05/2002','18062002')
